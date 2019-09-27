0537b0cbcd30f53890d8cf435e3b77fd175ce259
raymondhechen <rc284@duke.edu>
2019-09-27 15:34:51 -0400

    Added select DB functionality

--- flask-backend/app/badrank.py
+++ flask-backend/app/badrank.py
@@ -8,19 +8,19 @@ from flask import redirect # for update
 
 from flask_sqlalchemy import SQLAlchemy
 
+# INITIAL SETUP --> START WITH ms.db
 project_dir = os.path.dirname(os.path.abspath(__file__)) # Project dir
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "ms.db")) # Database loc
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = database_file 
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app) # Initialize database connection
 
+
+# DATABASE STRUCTURES
 class Player(db.Model):
     elo = db.Column(db.Integer, nullable=False) # ELO Data
     name = db.Column(db.String(80), unique=True, nullable=False, primary_key=True) # Name Data
-
-    def __repr__(self):
-        return "<Name: {}>".format(self.name)
 
 class PlayerGame(db.Model):
     # Assume p1 is winner, p2 is loser
@@ -32,6 +32,16 @@ class PlayerGame(db.Model):
     g2p2 = db.Column(db.Integer, nullable=False)
     g3p1 = db.Column(db.Integer, nullable=True)
     g3p2 = db.Column(db.Integer, nullable=True)
+
+
+# MEHTODS
+@app.route("/setDB", methods=["GET","POST"])
+def selectDB():
+    dbName = request.form.get("dbName") + ".db"
+    project_dir = os.path.dirname(os.path.abspath(__file__)) # Project dir
+    database_file = "sqlite:///{}".format(os.path.join(project_dir, dbName))
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+    return redirect("/")
 
 
 @app.route("/", methods=["GET","POST"])
@@ -79,7 +89,7 @@ def probability(rating1, rating2):
   
 # Function to calculate Elo rating 
 # k is a constant
-# d determines whether Pa wins or Pb wins
+# d determines whether Pa wins or Pb wins (1 or 0)
 def eloRating(Ra, Rb, k, d):
     Pb = probability(Ra, Rb) # Calculate the winning probability of Player B 
     Pa = probability(Rb, Ra) # Clculate the Winning probability of Player A 
